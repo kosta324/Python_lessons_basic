bb1188f78cfc83a4d31a228db098f4ac21a3a422
kosta324 <kosta324@yandex.ru>
2018-08-08 01:05:44 +0300

    урок 1 нормал задача 2 из пастебина

---
+++ lesson01/home_work/Python-1-lesson1-normal-task2-pastebin-Korotkov-Konstantin.py
@@ -0,0 +1,8 @@
+__author__ = 'Korotkov Konstantin'
+
+namber_1 = int(input('Введите переменную 1: '))
+namber_2 = int(input('Введите переменную 2: '))
+namber_1 = namber_1 + namber_2
+namber_2 = namber_1 - namber_2
+namber_1 = namber_1 - namber_2
+print('Значения переменных поменяны: переменная 1 =', namber_1, 'переменная 2 =', namber_2)
