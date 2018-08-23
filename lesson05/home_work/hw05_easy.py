483e8cb8cfea16a4d4daedb9cfabb74fc0e81a48
kosta324 <kosta324@yandex.ru>
2018-08-23 19:25:55 +0300

    easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,29 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+import sys
+
+for i in range(9):
+    dir_path = os.path.join(os.getcwd(), 'dir_' + str(i + 1))
+    try:
+        os.mkdir(dir_path)
+    except FileExistsError:
+        print('Такая папка существует ' + dir_path)
+
+for i in range(9):
+    dir_path = os.path.join(os.getcwd(), 'dir_' + str(i + 1))
+    try:
+        os.rmdir(dir_path)
+    except OSError:
+        print('Папку нельзя удалить ' + dir_path)
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+for i, dir, file in os.walk(os.getcwd()):
+    print(i)
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
